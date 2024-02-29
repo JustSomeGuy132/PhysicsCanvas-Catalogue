723699280e5c2e58d6c04543e54519c2bc051f4a
JustSomeGuy132 <hossainadon@gmail.com>
2024-02-29 23:29:13 +0000

    Current state with database kind of working

--- app.js
+++ app.js
@@ -6,38 +6,66 @@ const path = require('path');
 const db = new sqlite3.Database(path.join(__dirname, 'cat_db.db'), sqlite3.OPEN_READWRITE, 
     (err)=>{
         if(err){
-            alert("Database error");
-            console.log("Database error");
+            console.log("Database error:\n" + err);
         }
     }
 );
 publicPath = path.join(__dirname, 'public');
 
 app.use(express.json());
-app.use(express.static(publicPath));
 
 app.post('/signup', (req, res)=>{
     const {email, username, password} = req.body;
-    console.log("Received a signup request");
-    db.all(`SELECT * FROM Users WHERE Email = '?' AND Username = '?' AND Password = '?';`,
-        [email, username, password],
-        (err, rows)=>{
-            if (err){ alert("DB ERROR"); }
-            else {
-                if (rows.length === 0) {
-                    db.run(`INSERT INTO Users VALUES('?', '?', '?');`,
-                    [email, username, password]);
-                    res.status(200).send('Account created successfully!');
-                }
+    db.get(`SELECT * FROM Users WHERE Email = '${email}';`,
+        (err, row)=>{
+            if (err){ 
+                res.status(200).send("Error in select statement:\n" + err.message); 
+                return; 
             }
+            console.log("Grabbing rows...");
+            console.log("Row retrieved: " + row);
+            if (row) {
+                res.status(200).send("User by this email address already exists.");
+                return;
+            }
+            console.log("Going to insert now for " + email + ", " + username + ", " + password);
+
+            db.run(`INSERT INTO Users VALUES('${email}', '${username}', '${password}');`,
+            (err)=>{
+                if(err){
+                    console.error("Error when inserting:\n" + err);
+                    return;
+                }
+                else{
+                    console.log("No error when inserting");
+                }
+            });
+            res.status(200).send('Account created successfully!');
+            
         }
     );
-    
 });
 
 app.post('/login', (req,res)=>{
     const {email, password} = req.body;
-
+    console.log("Logging in to " + email);
+    db.get(`SELECT * FROM Users WHERE Email = '${email}' AND '${password}';`,
+    (err, row)=>{
+        if(err){
+            console.error("Internal server error: " + err);
+            res.status(200).send(err);
+            return;
+        }
+        console.log("Row retrieved: " + row);
+        if(!row){
+            res.status(200).send('No such user exists');
+            return;
+        }
+        res.status(200).send('Login successful!');
+        console.log("Logged in to (" + email + ", " + password + ")");
+    })
 });
+
+app.use(express.static(publicPath));
 
 app.listen(8080, ()=>{console.log("Listening to port 8080")});
--- public/acc_scripts.js
+++ public/acc_scripts.js
@@ -1,32 +1,53 @@
 function VerifyLogin(){
-    
-}
-
-function ValidateSignUp(){
-    var email = document.getElementById("email-sign").value;
-    var usern = document.getElementById("username").value;
-    var pass = document.getElementById("password-sign").value;
-    var re_pass = document.getElementById("re-password").value;
-    if(!email || !usern){
-        alert("Email or username not entererd!");
-        return;
-    }
-    if (pass.length < 7 || pass.length > 20){
-        alert("Password invalid, length detected = " + pass.length);
-        return;
-    }
-    if (pass !== re_pass){
-        alert("Passwords do not match!");
-        return;
-    }
-    
-    fetch('/signup', 
+    const email = document.getElementById("email-log").value;
+    const password = document.getElementById("password-log").value;
+    fetch('../login',
     {
         method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({email, usern, pass})
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email, password})
     })
     .then(response => response.text())
     .then(message => alert(message))
     .catch(error => alert(error));
+}
+
+function ValidUserN(){
+    const usern = document.getElementById("username").value;
+    return (usern.length > 0 && usern.length <= 20);
+}
+function ValidPassword(){
+    const pass = document.getElementById("password-sign").value;
+    return (pass.length > 7 && pass.length <= 20);
+}
+function ValidRePass(){
+    return (document.getElementById("password-sign").value === document.getElementById("re-password").value);
+}
+
+function ValidateSignUp(){
+    const email = document.getElementById("email-sign").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password-sign").value;
+    if(!email || !ValidUserN()){
+        alert("Email or username are invalid!");
+        return;
+    }
+    if (!ValidPassword()){
+        alert("Password length invalid, length detected = " + pass.length);
+        return;
+    }
+    if (!ValidRePass()){
+        alert("Passwords do not match!");
+        return;
+    }
+    
+    fetch('../signup',
+    {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({email, username, password})
+    })
+    .then(response => response.text())
+    .then(message => alert(message))
+    .catch(error => alert("Error after fetch:\n" + error));
 }
